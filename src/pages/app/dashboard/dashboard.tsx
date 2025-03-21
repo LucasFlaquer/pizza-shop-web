c461fa4e7eb684342179a5047f29552775ca7017
lucasflaquer <lucas.flaquer@gmail.com>
2025-03-20 23:23:43 -0300

    popular products chart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { DayOrdersAmountCard } from '@/pages/app/dashboard/day-orders-amount-car
 import { MonthCanceledOrdersAmountCard } from '@/pages/app/dashboard/month-canceled-orders-amount-card'
 import { MonthOrdersAmountCard } from '@/pages/app/dashboard/month-orders-amount-card'
 import { MonthRevenueCard } from '@/pages/app/dashboard/month-revenue-card'
+import { PopularProductsChart } from '@/pages/app/dashboard/popular-products-chart'
 import { RevenueChart } from '@/pages/app/dashboard/revenue-chart'
 
 export function Dashboard() {
@@ -20,6 +21,7 @@ export function Dashboard() {
         </div>
         <div className="grid grid-cols-9 gap-4">
           <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </>
